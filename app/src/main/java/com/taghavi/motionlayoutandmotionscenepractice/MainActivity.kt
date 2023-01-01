10cc096f6c59f260626b6fa965528f6358dfa7ac
EhsanTgv <EhsanTgv@gmail.com>
2023-01-02 00:46:26 +0300

    Implement motion

--- app/src/main/java/com/taghavi/motionlayoutandmotionscenepractice/MainActivity.kt
+++ app/src/main/java/com/taghavi/motionlayoutandmotionscenepractice/MainActivity.kt
@@ -1,11 +1,23 @@
 package com.taghavi.motionlayoutandmotionscenepractice
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Color
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import com.google.android.material.appbar.CollapsingToolbarLayout
+import com.taghavi.motionlayoutandmotionscenepractice.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        setSupportActionBar(binding.toolbar)
+
+        binding.collapsingToolbar.setContentScrimColor(Color.GREEN)
     }
 }
